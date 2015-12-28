057dc777bb43d64a01a331d35e1bd54f64c663da
Loganathan Ramasamy <loganathanspr@gmail.com>
2015-12-28 12:50:04 +0100

    Language parameter and maximum edit distance  parameters.

--- run/stage4.sh
+++ run/stage4.sh
@@ -12,10 +12,15 @@ output_dir=$3
 # Directory where the wiki-error-corpus github files are located
 code_root=$4
 
+# Language of the text content. Needed for segmenting/tokenizing the text.
+lang=$5
+
+# Maximum edit distance between the correct word and misspelled word.
+max_edit=$6
 
 input_file_path=${input_dir}/${input_file}
 output_file_path=${output_dir}/${input_file}
 corpus_dir=${input_dir}
 
-python ${code_root}/reader/extract_spelling_errors.py ${corpus_dir} ${input_file} ${output_dir}
+python ${code_root}/reader/extract_spelling_errors.py ${corpus_dir} ${input_file} ${output_dir} ${lang} ${max_edit}
 
